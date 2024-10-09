63516cac0b700b1eed885396fd2befa4f5022b15
Siddique <ahmedsiddique552@gmail.com>
2024-10-09 18:02:04 +0600

    date of birth section development start

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,6 +1,7 @@
 import { LiaTimesSolid } from "react-icons/lia";
 import "./Modal.scss";
 import { useState } from "react";
+import { FaQuestionCircle } from "react-icons/fa";
 
 const Modal = ({ hide }) => {
   return (
@@ -12,17 +13,41 @@ const Modal = ({ hide }) => {
               <h2>Create a new account</h2>
               <p>It's quick and easy.</p>
             </div>
-            <i><LiaTimesSolid onClick={() => hide(false)}></LiaTimesSolid></i>
+            <i>
+              <LiaTimesSolid onClick={() => hide(false)}></LiaTimesSolid>
+            </i>
           </div>
           <div className="modal-body">
             <div className="login-form">
               <form action="">
                 <div className="name-input">
-                <input type="text" placeholder="First Name"/>
-                <input type="text" placeholder="Surname"/>
+                  <input type="text" placeholder="First Name" />
+                  <input type="text" placeholder="Surname" />
                 </div>
-                <input type="text"  placeholder="Mobile number or email adress"/>
-                <input type="text"  placeholder="New Password"/>
+                <input
+                  type="text"
+                  placeholder="Mobile number or email adress"
+                />
+                <input type="text" placeholder="New Password" />
+                <div className="reg-extra">
+                  <div className="reg-extra-title">
+                    <h6>Date of birth</h6>
+                    <i>
+                      <FaQuestionCircle />
+                    </i>
+                  </div>
+                  <div className="dmy">
+                  <select name="" id="">
+                    <option value="">Day</option>
+                  </select>
+                  <select name="" id="">
+                    <option value="">Month</option>
+                  </select>
+                  <select name="" id="">
+                    <option value="">Year</option>
+                  </select>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
